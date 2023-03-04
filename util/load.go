8adf2539f0a0c995d115c9ee63a32b3b468a6010
myo-i <Imiyo0521@gmail.com>
2023-03-05 02:08:50 +0900

    ✨ 環境設定ファイルから環境変数を取得

---
+++ util/load.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DbPath string
+}
+
+// 環境変数を取得
+func Load() (Config, error) {
+	viper.SetConfigName("db")
+	viper.SetConfigType("env")
+	viper.AddConfigPath("./")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Failed to load env file: %v", err)
+	}
+
+	var config Config
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		log.Fatalf("Failed to ")
+	}
+
+	return config, err
+}
