d9e54542a255562405a8ca8fc72a0515e8d990ee
myo-i <Imiyo0521@gmail.com>
2023-03-12 23:56:07 +0900

    ⚡️ テキストとグラフを関数にまとめた

--- user/root.go
+++ user/root.go
@@ -53,55 +53,12 @@ func RunApp() {
 
 	rect2.Resize(fyne.NewSize(50, 125))
 
+	window1Week := Democreate1DayGraph()
+
 	/////////////     やりたいレイアウトはこれ！！！！   /////////////
 	// 後はポジションを動的にする!!!!
-	labelWeek := widget.NewLabel("1Week")
-	labelDay := widget.NewLabel("1Day")
-	labelWeek.Move(fyne.NewPos(30, 30))
 
-	rect4 := canvas.NewRectangle(green)
-
-	text4 := canvas.NewText("test", green)
-
-	containerBarText := container.NewWithoutLayout(
-		rect4,
-		text4,
-	)
-	containerBarText.Resize(fyne.NewSize(30, 150))
-	rect4.Resize(fyne.NewSize(containerBarText.Size().Width, 80))
-	text4.Resize(fyne.NewSize(containerBarText.Size().Width, 30))
-	containerBarText.Move(fyne.NewPos(100, 100))
-	rect4.Move(fyne.NewPos(containerBarText.Position().X, containerBarText.Size().Height-rect4.Size().Height))
-	text4.Move(fyne.NewPos(containerBarText.Position().X, containerBarText.Size().Height))
-
-	// content2
-	rect5 := canvas.NewRectangle(color.Opaque)
-
-	text5 := canvas.NewText("test", green)
-
-	containerBarText2 := container.NewWithoutLayout(
-		rect5,
-		text5,
-	)
-	containerBarText2.Resize(fyne.NewSize(30, 150))
-	rect5.Resize(fyne.NewSize(containerBarText2.Size().Width, 120))
-	text5.Resize(fyne.NewSize(containerBarText2.Size().Width, 30))
-	containerBarText2.Move(fyne.NewPos(150, 100))
-	rect5.Move(fyne.NewPos(containerBarText2.Position().X, containerBarText2.Size().Height-rect5.Size().Height))
-	text5.Move(fyne.NewPos(containerBarText2.Position().X, containerBarText2.Size().Height))
-
-	// border
-
-	window1Week := container.NewWithoutLayout(
-		// 同じCanvasObjectを複数個入れても同一のものとみなされる
-		labelWeek,
-		labelDay,
-		containerBarText,
-		containerBarText2,
-	)
 	/////////////////////////////////////////////////////////////
-	window1Week.Resize(fyne.NewSize(450, 300))
-	rect2.Resize(fyne.NewSize(window1Week.Size().Width-50, window1Week.Size().Height-50))
 
 	tab := container.NewAppTabs(
 		container.NewTabItem("1Day", window1Day),
@@ -155,6 +112,75 @@ func create1DayGraph(c fyne.Canvas) {
 	// // line.Move(line.Position())
 	// fmt.Println(line.Position())
 	// c.SetContent(line)
+}
+
+func Democreate1DayGraph() fyne.CanvasObject {
+	green := color.NRGBA{R: 0x43, G: 0xff, B: 0x64, A: 0xd9}
+	/////// content1
+	rect4 := canvas.NewRectangle(green)
+
+	text4 := canvas.NewText("test", green)
+
+	containerBarText := container.NewWithoutLayout(
+		rect4,
+		text4,
+	)
+	containerBarText.Resize(fyne.NewSize(30, 150))
+	rect4.Resize(fyne.NewSize(containerBarText.Size().Width, 80))
+	text4.Resize(fyne.NewSize(containerBarText.Size().Width, 30))
+	containerBarText.Move(fyne.NewPos(100, 100))
+	rect4.Move(fyne.NewPos(containerBarText.Position().X, containerBarText.Size().Height-rect4.Size().Height))
+	text4.Move(fyne.NewPos(containerBarText.Position().X, containerBarText.Size().Height))
+
+	/////// content2
+	rect5 := canvas.NewRectangle(color.Opaque)
+
+	text5 := canvas.NewText("test", green)
+
+	containerBarText2 := container.NewWithoutLayout(
+		rect5,
+		text5,
+	)
+	containerBarText2.Resize(fyne.NewSize(30, 150))
+	rect5.Resize(fyne.NewSize(containerBarText2.Size().Width, 120))
+	text5.Resize(fyne.NewSize(containerBarText2.Size().Width, 30))
+	containerBarText2.Move(fyne.NewPos(150, 100))
+	rect5.Move(fyne.NewPos(containerBarText2.Position().X, containerBarText2.Size().Height-rect5.Size().Height))
+	text5.Move(fyne.NewPos(containerBarText2.Position().X, containerBarText2.Size().Height))
+
+	/////// content3
+	yellow := color.NRGBA{R: 0xf2, G: 0xff, B: 0x00, A: 0xff}
+	rect6 := canvas.NewRectangle(yellow)
+
+	text6 := canvas.NewText("test", yellow)
+
+	containerBarText3 := container.NewWithoutLayout(
+		rect6,
+		text6,
+	)
+	containerBarText3.Resize(fyne.NewSize(30, 150))
+	rect6.Resize(fyne.NewSize(containerBarText3.Size().Width, 100))
+	text6.Resize(fyne.NewSize(containerBarText3.Size().Width, 30))
+	containerBarText3.Move(fyne.NewPos(200, 100))
+	rect6.Move(fyne.NewPos(containerBarText3.Position().X, containerBarText3.Size().Height-rect6.Size().Height))
+	text6.Move(fyne.NewPos(containerBarText3.Position().X, containerBarText3.Size().Height))
+
+	// border
+	labelWeek := widget.NewLabel("1Week")
+	labelDay := widget.NewLabel("1Day")
+	labelWeek.Move(fyne.NewPos(30, 30))
+
+	window1Week := container.NewWithoutLayout(
+		// 同じCanvasObjectを複数個入れても同一のものとみなされる
+		labelWeek,
+		labelDay,
+		containerBarText,
+		containerBarText2,
+		containerBarText3,
+	)
+
+	return window1Week
+
 }
 
 func create1WeekGraph() fyne.CanvasObject {
