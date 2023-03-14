ffeb76f3cf9830de829144851ee223b0e4115c12
myo-i <Imiyo0521@gmail.com>
2023-03-15 04:00:22 +0900

    🚧 とりあえず流れだけメモ

--- db/db.go
+++ db/db.go
@@ -3,7 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	"gorail/util"
+	"gorail/config"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -15,9 +15,17 @@ type Urls struct {
 	LastVisit int
 }
 
+// とりあえず3か月分くらいのデータを（できれば滞在時間が長い順で）取得する
+// 取得したデータを1日、1週間、1か月とかでさらに範囲を絞って使う
+
+type History interface {
+	GetHistory()
+}
+
+// DBからデータを取得
 func GetData() {
 	// 環境変数取得
-	config, err := util.Load()
+	config, err := config.Load()
 	if err != nil {
 		log.Fatalf("Failed to load env file: %v", err)
 	}
@@ -36,6 +44,7 @@ func GetData() {
 	if err != nil {
 		log.Fatalf("Failed to execute query: %v", err)
 	}
+
 	// データ配列に格納
 	for rows.Next() {
 		var urls Urls
